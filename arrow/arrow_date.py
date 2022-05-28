0a450f5cc88a7bbfb15f1e64881220c4da5cad61
pritisolanki <pritiatwork@gmail.com>
2022-05-28 21:50:32 +0530

    Task: Add support of Poetry

--- arrow/arrow_date.py
+++ arrow/arrow_date.py
@@ -1,10 +1,12 @@
 import arrow
 
-#print arrow installed version
+#print arrow installed version - By Priti
 print(arrow.__version__ )
 
 #format current date with timezone and AM/PM 
 print(f'current time in PST: { arrow.now("US/Pacific").format("MMMM DD, YYYY HH:mm A")}');
 print(f'current time in EST: { arrow.now("America/New_York").format("MMMM DD, YYYY HH:mm A")}');
 
-
+todaydate=arrow.now("America/New_York")
+print(f'Today is { todaydate.format("dddd MMMM Do")} and time is {todaydate.format("HH:mm A")}');
+print(f'my current timezone is : {arrow.now().format("ZZZ")}')
